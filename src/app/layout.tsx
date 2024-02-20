05d77e9ee48eef0c335674f15677af6779694b61
MuhammadUsamaAwan <underusama@gmail.com>
2024-02-20 11:53:31 +0500

    chore: theme, tooltip & toast provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,9 @@ import type { Metadata, Viewport } from 'next';
 import { siteConfig } from '~/config/site';
 import { fontSans } from '~/lib/fonts';
 import { absoluteUrl, cn } from '~/lib/utils';
+import { Toaster } from '~/components/ui/toaster';
+import { TooltipProvider } from '~/components/ui/tooltip';
+import { ThemeProvider } from '~/components/layouts/theme-provider';
 
 import '~/styles/globals.css';
 
@@ -26,7 +29,12 @@ export const viewport: Viewport = {
 export default function RootLayout({ children }: React.PropsWithChildren) {
   return (
     <html lang='en' suppressHydrationWarning>
-      <body className={cn('flex min-h-dvh flex-col font-sans antialiased', fontSans.variable)}>{children}</body>
+      <body className={cn('flex min-h-dvh flex-col font-sans antialiased', fontSans.variable)}>
+        <ThemeProvider attribute='class' defaultTheme='light' enableSystem disableTransitionOnChange>
+          <TooltipProvider delayDuration={500}>{children}</TooltipProvider>
+          <Toaster />
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
