53d51b4eb130036ce78ae2382c65e3cf2e2a5566
MuhammadUsamaAwan <underusama@gmail.com>
2024-02-20 11:43:15 +0500

    chore: add env

---
+++ src/env.js
@@ -0,0 +1,41 @@
+import { createEnv } from '@t3-oss/env-nextjs';
+import { z } from 'zod';
+
+export const env = createEnv({
+  server: {
+    NODE_ENV: z.enum(['development', 'test', 'production']).default('development'),
+    DATABASE_USER: z.string(),
+    DATABASE_PASSWORD: z.string(),
+    DATABASE_HOST: z.string(),
+    DATABASE_PORT: z.string(),
+    DATABASE_NAME: z.string(),
+    AUTH_SECRET: z.string(),
+    GOOGLE_CLIENT_SECRET: z.string(),
+    AUTHORIZE_NET_API_LOGIN_ID: z.string(),
+    AUTHORIZE_NET_TRANSACTION_KEY: z.string(),
+    AUTHORIZE_NET_SANDBOX: z.string(),
+  },
+  client: {
+    NEXT_PUBLIC_APP_URL: z.string(),
+    NEXT_PUBLIC_GOOGLE_CLIENT_ID: z.string(),
+    NEXT_PUBLIC_FILE_URL: z.string(),
+  },
+  runtimeEnv: {
+    NODE_ENV: process.env.NODE_ENV,
+    DATABASE_USER: process.env.DATABASE_USER,
+    DATABASE_PASSWORD: process.env.DATABASE_PASSWORD,
+    DATABASE_HOST: process.env.DATABASE_HOST,
+    DATABASE_PORT: process.env.DATABASE_PORT,
+    DATABASE_NAME: process.env.DATABASE_NAME,
+    AUTH_SECRET: process.env.AUTH_SECRET,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+    AUTHORIZE_NET_API_LOGIN_ID: process.env.AUTHORIZE_NET_API_LOGIN_ID,
+    AUTHORIZE_NET_TRANSACTION_KEY: process.env.AUTHORIZE_NET_TRANSACTION_KEY,
+    AUTHORIZE_NET_SANDBOX: process.env.AUTHORIZE_NET_SANDBOX,
+    NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
+    NEXT_PUBLIC_GOOGLE_CLIENT_ID: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
+    NEXT_PUBLIC_FILE_URL: process.env.NEXT_PUBLIC_FILE_URL,
+  },
+  skipValidation: !!process.env.SKIP_ENV_VALIDATION,
+  emptyStringAsUndefined: true,
+});
